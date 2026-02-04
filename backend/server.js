5d84130d3953f430bf5f4e6ba4547b09b968b09c
Anjana Baskaran <anjanabaskaran2005@gmail.com>
2026-02-04 18:40:59 +0530

    Backend update

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,18 @@ import authRoutes from './routes/auth.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173", 
-    "http://localhost:3000",
-    "https://user-auth-8f3d8.web.app"
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://user-auth-8f3d8.firebaseapp.com",
+      "https://user-auth-8f3d8.web.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/account_creation')
